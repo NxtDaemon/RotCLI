f1c19421c910f6abc8e5188cd660a9f91d2334fc
NxtDaemon <Lukecotterell11@gmail.com>
2021-11-01 23:05:32 +0000

    fixed method + cool output

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# RotCLI 
-RotCLI allows users to do rotational ciphers where K = 0 -> 26  ; this allows for streamline bruteforcing <br>
-this also allows you to do specific rotations and this maybe be added with multiple types of decryption 
+# RotCLI
 
+NOTE: this maybe be added with multiple types of decryption <br><br>
+RotCLI allows users to do rotational ciphers where K = 0 -> 26 ; this allows for streamlined bruteforcing <br>
+this also allows you to do specific rotations.
 
 ## Usage
-`python3 RotCLI.py -m "message" -N key` 
-as a default this code with use N=13 or the same as Rot-13 <br>
 
-## Flags 
+```
+python3 RotCLI.py -m "message" -N key
+python3 Rot.py -M "$(cat ../output)" -B
+```
+
+This script uses argparse to parse the input, due to the fact this will mostly by used while in a bash or zsh shell I have decided to not add an input file section to achieve the same result you can use the above syntax. As a default this code will use N=13 if the value of N is not given alongside it's flag e.g. `-N K` <br>
+
+## Flags
+
 ```
 -B | Bruteforce
 -M | Message
@@ -15,13 +22,26 @@ as a default this code with use N=13 or the same as Rot-13 <br>
 ```
 
 ## Caesar Algorithm Implementation
-this code follows the standard caesar algorithm <br>
+
+This code follows the standard caesar algorithm <br>
 
 ```
-C[i] - i-th character of the closed text <br>
-T[i] - i-th character of the open text <br>
-k - shift <br>
-m - length of the alphabet <br>
+C[i] - i-th character of the ciphertext
+P[i] - i-th character of the plaintext
+k - Shift value
+m - Length of the alphabet (26)
 ```
 
-`T[i] = (C[i] - k) mod M`
+This means encryption is done using the following <br>
+
+```
+T[i] = (C[i + k % 26)]
+```
+
+hence decryption is done using <br>
+
+```
+P[i] = T[i - (k % 26)]
+```
+
+it is worth noting that due to how most decryption methods work, they use wrap around values only using the encryption algorithm, since that K=-12 is the same as K=14 hence to fit in with this commonplace the script only uses the first mentioned method. <br>
--- Rot.py
+++ Rot.py
@@ -1,16 +1,22 @@
 import argparse
 import string
+import sys
+import re
 
-# Argparse setup 
+# Argparse setup
 Parser = argparse.ArgumentParser()
-Parser.add_argument("-m",help="pass the Message to run through the rotational cipher",nargs="+",required=True)
-Parser.add_argument("-N",help="Pass the N-rotational value [Default is 13]",default=13,type=int)
-Parser.add_argument("-B",help="Switch for bruteforce the N-val",action='store_true')
+Parser.add_argument(
+    "-M", help="pass the Message to run through the rotational cipher", required=True, type=str)
+Parser.add_argument(
+    "-N", help="Pass the N-rotational value [Default is 13]", default=13,  type=int)
+Parser.add_argument(
+    "-B", help="Switch for bruteforce the N-val", action='store_true')
 
 # Move a argparse value into a stored variable
 Args = Parser.parse_args()
-Message = Args.m
+Message = Args.M
 N = Args.N
+
 
 class Color:
     'Class for Colors to be used in Execution'
@@ -26,89 +32,96 @@ class Color:
 
     QuestionColor = BOLD+YELLOW
     ErrorColor = RED+BOLD
-    InfoColor = CYAN 
+    InfoColor = CYAN
     SuccessColor = GREEN
 
+
 class Notify():
-   'Managed what type of message is sent'
+    'Managed what type of message is sent'
 
-   def Error(Message):
-      'Error Messages'
-      print(f"{Color.ErrorColor}[!] - {Message}{Color.RESET}")
+    def KeyOutput(Key, Message):
+        'Key Output'
+        Message = Message.replace("\n", " ").replace("\r", " ")
+        Message = re.findall(r"[\s\S]{0,110}", Message)
+        print(f"{Color.YELLOW}[🔑] -> {Key}{Color.RESET}")
+        for _ in Message:
+            print(" "*8 + _)
 
-   def Info(Message):
-      'Infomation Messages'
-      print(f"{Color.InfoColor}[*] - {Message}{Color.RESET}")
+    def Error(Message):
+        'Error Messages'
+        print(f"{Color.ErrorColor}[!] - {Message}{Color.RESET}")
 
-   def Success(Message):
-      'Success Messages'
-      print(f"{Color.SuccessColor}[$] - {Message}{Color.RESET}")
+    def Info(Message):
+        'Infomation Messages'
+        print(f"{Color.InfoColor}[*] - {Message}{Color.RESET}")
 
-   def Question(Message):
-      'Get infomation from user'
-      input(f"{Color.QuestionColor}[?] - {Message}{Color.RESET}")
+    def Success(Message):
+        'Success Messages'
+        print(f"{Color.SuccessColor}[$] - {Message}{Color.RESET}")
+
+    def Question(Message):
+        'Get infomation from user'
+        input(f"{Color.QuestionColor}[?] - {Message}{Color.RESET}")
 
 
-def GetLetter(List,char,key):
-   x = (List.index(char) - key) % 26
-   Letter = List[x]
-   return(Letter)
+def GetLetter(List, char, key):
+    x = (List.index(char) + key) % 26
+    Letter = List[x]
+    return(Letter)
 
 
 class Decrypt:
-   'Class for decrypting rotational ciphers'
-   def __init__(self,Message,N):
-      'init message and N-value'
-      self.Message = Message
-      self.N = N % 26
+    'Class for decrypting rotational ciphers'
 
-   def Bruteforce(self):
-      TranslatedList = []
-      for key in range(0,26): 
-         Translated = ""
-         for char in str(Message):
+    def __init__(self, Message, N):
+        'init message and N-value'
+        self.Message = Message
+        self.N = N % 26
 
+    def Bruteforce(self):
+        TranslatedList = []
+        for key in range(0, 26):
+            Translated = ""
+            for char in str(Message):
+                if char.isalpha():
+                    if char.isupper():
+                        Letter = GetLetter(string.ascii_uppercase, char, key)
+                        Translated += Letter
+                    elif char.islower():
+                        Letter = GetLetter(string.ascii_lowercase, char, key)
+                        Translated += Letter
+                else:
+                    Translated += char
+            TranslatedList.append([f"{key}", f"{Translated}"])
+        for _ in TranslatedList:
+            key = _[0].rjust(2)
+            value = str(_[1])
+            Notify.KeyOutput(key, value)
+
+    def Standard(self):
+        Translated = ""
+        key = N
+
+        for char in str(Message):
             if char.isalpha():
-               if char.isupper():
-                  Letter = GetLetter(string.ascii_uppercase,char,key)
-                  Translated += Letter
-               elif char.islower():
-                  Letter = GetLetter(string.ascii_lowercase,char,key)
-                  Translated += Letter
+                if char.isupper():
+                    Letter = GetLetter(string.ascii_uppercase, char, key)
+                    Translated += Letter
+                elif char.islower():
+                    Letter = GetLetter(string.ascii_lowercase, char, key)
+                    Translated += Letter
             else:
-               Translated += char    
-
-         TranslatedList.append([f"{key}",f"{Translated}"]) 
-      for _ in TranslatedList:
-         key = _[0].rjust(2)
-         value = str(_[1])
-         Notify.Info(f"{key} : {value}")
-
-   def Standard(self):
-      Translated = ""
-      key = N
-      for char in str(Message):
-         if char.isalpha():
-            if char.isupper():
-               Letter = GetLetter(string.ascii_uppercase,char,key)
-               Translated += Letter
-            elif char.islower():
-               Letter = GetLetter(string.ascii_lowercase,char,key)
-               Translated += Letter
-         else:
-            Translated += char
-      Notify.Info(f"{N} : {Translated}")
+                Translated += char
+        Notify.KeyOutput(N, Translated)
 
 
-def Main(Message,N):
-   if Message:
-      Message = " ".join(Message)
-   D = Decrypt(Message,N)
-   if Args.B:
-      D.Bruteforce()
-   else:
-      D.Standard()
+def Main(Message, N):
+    D = Decrypt(Message, N)
+    if Args.B:
+        D.Bruteforce()
+    else:
+        D.Standard()
 
 
 if __name__ == "__main__":
-      Main(Message,N)
+    Main(Message, N)
