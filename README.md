fc562c8ccd59a8f3bc69acaca30fce7ddf4aabbf
NxtDaemon <Lukecotterell11@gmail.com>
2021-11-03 20:04:09 +0000

    Adding Letter Goodness

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ this also allows you to do specific rotations.
 ## Usage
 
 ```
-python3 RotCLI.py -m "message" -N key
-python3 Rot.py -M "$(cat ../output)" -B
+python3 RotCLI.py -m "message" -N key # For direct N=key
+python3 Rot.py -M "$(cat ../output)" -B # Bruteforcing All Keys
+python3 Rot.py -M "$(cat test.txt)" -B -L # For Large Sample Size
 ```
 
 This script uses argparse to parse the input, due to the fact this will mostly by used while in a bash or zsh shell I have decided to not add an input file section to achieve the same result you can use the above syntax. As a default this code will use N=13 if the value of N is not given alongside it's flag e.g. `-N K` <br>
@@ -18,9 +19,10 @@ This script uses argparse to parse the input, due to the fact this will mostly b
 ## Flags
 
 ```
--B | Bruteforce
--M | Message
--N | Rotational Value / Key
+-M | Pass the Message to run through the rotational cipher
+-N | Pass the N-rotational value [Default is 13]
+-B | Switch for bruteforce the N-val
+-L | Switch for Letter-Goodness
 ```
 
 <br>
@@ -35,6 +37,25 @@ python3 Rot.py -M "$(cat ../file)" -N 14
         co laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
          in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat c
         upidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+```
+
+```
+python3 Rot.py -M "$(cat test.txt)" -B -L
+[*] - Printing Message with Highest Letter Goodness
+
+[🔑] -> 19 [43.06239999999993]
+        Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph
+        is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and
+         coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sen
+        tences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determ
+        ine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journal
+        istic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sen
+        tences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because i
+        t controls what happens in the rest of the paragraph.
+
+[?] - Do you Want to Print the Next Value? q
+
+[$] - Hope you Got your Result ／人◕ __ ◕人＼
 ```
 
 <br>
--- Rot.py
+++ Rot.py
@@ -2,15 +2,18 @@ import argparse
 import string
 import sys
 import re
+import string
 
 # Argparse setup
 Parser = argparse.ArgumentParser()
 Parser.add_argument(
-    "-M", help="pass the Message to run through the rotational cipher", required=True, type=str)
+    "-M", help="Pass the Message to run through the rotational cipher", required=True, type=str)
 Parser.add_argument(
     "-N", help="Pass the N-rotational value [Default is 13]", default=13,  type=int)
 Parser.add_argument(
     "-B", help="Switch for bruteforce the N-val", action='store_true')
+Parser.add_argument(
+    "-L", help="Switch for Letter-Goodness", action='store_true')
 
 # Move a argparse value into a stored variable
 Args = Parser.parse_args()
@@ -39,11 +42,29 @@ class Color:
 class Notify():
     'Managed what type of message is sent'
 
-    def KeyOutput(Key, Message):
+    def LG_KeyOutput(LG_Dict):
+        Order = sorted(list(LG_Dict.keys()), reverse=True)
+        Notify.Info("Printing Message with Highest Letter Goodness\n")
+        AcceptList = ["YES", "Y"]
+        c = 0
+        while True:
+            Top = LG_Dict[Order[c]]
+            TopKey = Top[0]
+            TopValue = Top[1]
+            Notify.KeyOutput(TopKey, TopValue, Order[c])
+            Res = Notify.Question("Do you Want to Print the Next Value?")
+            if Res.upper() not in AcceptList:
+                Notify.Success("Hope you Got your Result ／人◕ __ ◕人＼")
+                break
+            c += 1
+
+    def KeyOutput(Key, Message, LG=""):
         'Key Output'
+        if LG:
+            LG = f"[{LG}]"
         Message = Message.replace("\n", " ").replace("\r", " ")
         Message = re.findall(r"[\s\S]{0,110}", Message)
-        print(f"{Color.YELLOW}[🔑] -> {Key}{Color.RESET}")
+        print(f"{Color.YELLOW}[🔑] -> {Key} {LG}{Color.RESET}")
         for _ in Message:
             print(" "*8 + _)
 
@@ -61,7 +82,9 @@ class Notify():
 
     def Question(Message):
         'Get infomation from user'
-        input(f"{Color.QuestionColor}[?] - {Message}{Color.RESET}")
+        tmp = input(f"{Color.QuestionColor}[?] - {Message}{Color.RESET}")
+        print()
+        return(tmp)
 
 
 def GetLetter(List, char, key):
@@ -73,16 +96,19 @@ def GetLetter(List, char, key):
 class Decrypt:
     'Class for decrypting rotational ciphers'
 
-    def __init__(self, Message, N):
+    def __init__(self, Message, N, LGS):
         'init message and N-value'
         self.Message = Message
         self.N = N % 26
+        self.LetterGoodnessSwitch = LGS
 
     def Bruteforce(self):
+        'Bruteforce the key for Args.Message'
         TranslatedList = []
+        LetterGoodness_TranslatedDict = {}
         for key in range(0, 26):
             Translated = ""
-            for char in str(Message):
+            for char in str(self.Message):
                 if char.isalpha():
                     if char.isupper():
                         Letter = GetLetter(string.ascii_uppercase, char, key)
@@ -94,11 +120,19 @@ class Decrypt:
                     Translated += char
             TranslatedList.append([f"{key}", f"{Translated}"])
         for _ in TranslatedList:
-            key = _[0].rjust(2)
-            value = str(_[1])
-            Notify.KeyOutput(key, value)
+            Key = _[0].rjust(2)
+            Message = str(_[1])
+            if self.LetterGoodnessSwitch:
+                LetterGoodness = self.LetterGoodnessEvaluation(Message)
+                LetterGoodness_TranslatedDict.update({LetterGoodness: _})
+            else:
+                Notify.KeyOutput(Key, Message)
+
+        if self.LetterGoodnessSwitch:
+            Notify.LG_KeyOutput(LetterGoodness_TranslatedDict)
 
     def Standard(self):
+        'Perform Caesar cipher of N=K or N=13 as default on Args.Message'
         Translated = ""
         key = N
 
@@ -112,11 +146,30 @@ class Decrypt:
                     Translated += Letter
             else:
                 Translated += char
-        Notify.KeyOutput(N, Translated)
+        if self.LetterGoodnessSwitch:
+            LG = self.LetterGoodnessEvaluation(Translated)
+        else:
+            LG = None
+
+        Notify.KeyOutput(N, Translated, LG)
+
+    def LetterGoodnessEvaluation(self, Message):
+        'Calculate English Letter Goodness for Message'
+        LetterGoodness_Score = 0
+        LetterGoodnessDict = dict(zip(string.ascii_uppercase,
+                                      [.0817, .0149, .0278, .0425, .1270, .0223, .0202,
+                                       .0609, .0697, .0015, .0077, .0402, .0241, .0675,
+                                       .0751, .0193, .0009, .0599, .0633, .0906, .0276,
+                                       .0098, .0236, .0015, .0197, .0007]))
+        for char in Message:
+            if char.upper() in string.ascii_uppercase:
+                LetterGoodness_Score += LetterGoodnessDict[char.upper()]
+
+        return(LetterGoodness_Score)
 
 
 def Main(Message, N):
-    D = Decrypt(Message, N)
+    D = Decrypt(Message, N, LGS=Args.L)
     if Args.B:
         D.Bruteforce()
     else:
